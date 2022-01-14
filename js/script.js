706e65e9df2a9cadfbd826b4867f757d138b3161
Kenesi Kornél <Ny19KenesiK@szgya.hu>
2022-01-14 13:12:34 +0100

    md

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
     let nevszoveg =  document.getElementById("ertek1").value;
     let termekszoveg = document.getElementById("ertek2").value;
     let darabszoveg = document.getElementById("ertek3").value;
-    let muv = document.getElementById("muvelet").value;
+    let muv = document.getElementById("muvelet");
     let eredmeny = 0;
     let kiiras = document.getElementById("kiiras");
 
@@ -12,6 +12,15 @@
 
     if (muv === "*") {
         eredmeny = x * y;
-    }
+        document.getElementById("ertek1").focus();
+
+        kiiras.innerText = `${x} ${muv} ${y} = ${eredmeny}`;
+    console.log(eredmeny);
+
      
+    document.getElementById("ertek1").value = ""; 
+    document.getElementById("ertek2").value = ""; 
+    document.getElementById("ertek3").value = ""; 
+    }
+    document.getElementById("ertek1").focus();
     
